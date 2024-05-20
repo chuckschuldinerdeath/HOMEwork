d3c168f18780be659640a87007c282236dfd5d51
unknown <murat.mma95@gmail.com>
2024-05-20 06:00:34 +0300

    Box

---
+++ src/lesson4/Box.java
@@ -0,0 +1,47 @@
+package lesson4;
+
+public class Box {
+
+    int size;
+    String color;
+    boolean IsOpen;
+    boolean IsEmpty;
+
+    Box(int size, String color, boolean IsOpen, boolean IsEmpty) {
+        this.size = size;
+        this.color = color;
+        this.IsOpen = IsOpen;
+        this.IsEmpty = IsEmpty;
+    }
+
+    void info() {
+        System.out.println("размер ящика " + size + "\nцвет " + color);
+    }
+
+//    void isLock(){
+//        System.out.println("Желаете открыть ящик? введите да или нет");
+//        Scanner scanner = new Scanner(System.in);
+//        String answer = scanner.nextLine();
+//        if(answer == "да"){
+//            System.out.println("Ящик открыт");
+//        }
+
+
+    void open() {
+        if (IsOpen) {
+            System.out.println("Ящик открыт");
+        } else {
+            System.out.println("Ящик закрыт");
+        }
+    }
+
+    void full() {
+        if (IsEmpty) {
+            System.out.println("Ящик пуст");
+        } else {
+            System.out.println("Ящик полон");
+        }
+    }
+
+}
+
